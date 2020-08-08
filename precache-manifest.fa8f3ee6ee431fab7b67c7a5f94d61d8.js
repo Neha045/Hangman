035435fa2297c7c319c5d31a25c7970beb8fcfb3
Neha Sonpure <sonpureneha@gmail.com>
2020-08-08 15:03:48 +0530

    Updates

---
+++ precache-manifest.fa8f3ee6ee431fab7b67c7a5f94d61d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c395f0d4360ed4b32450ac19ba1b59",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "627d1ac935f0f0d6f1d7",
+    "url": "/Hangman/static/css/main.ce81d9d6.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/Hangman/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hangman/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627d1ac935f0f0d6f1d7",
+    "url": "/Hangman/static/js/main.c1a3a987.chunk.js"
+  },
+  {
+    "revision": "8a93cdc09629d2b7005c",
+    "url": "/Hangman/static/js/runtime-main.707d265e.js"
+  }
+]);
